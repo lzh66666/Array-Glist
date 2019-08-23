233fbb40c4497ae3cf5a4ec7d4f11d7ad7459de4
lzh <luzehua@zhongbei.com>
2019-08-23 11:32:50 +0800

    广义表(定长顺序)

--- 广义表.cpp
+++ 广义表.cpp
@@ -1,162 +1,122 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string.h> 
+#include <stdlib.h>	
+#include <stdio.h>		
+#include <string.h>
 
-#define OVERFLOW -2
+#define TRUE 1
+#define FALSE 0
 #define OK 1
 #define ERROR 0
-
-typedef int Status; 
-
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<串S的操作开始<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#define INFEASIBLE -1
+#define OVERFLOW -2
  
-typedef struct{
-	char *ch;    //若是空串，则按串长分配存储区，否则ch为NULL 
-	int length;  //串长度 
-}SString;
-
-Status InitSString(SString &T){
-	T.ch=(char *)malloc(sizeof(char)); 
-	T.length=0;
-	return OK;
-}
-
-Status StrAssign(SString &T,char *chars){
-//	生成一个其值等于串常量chars的串T 
-	int i=0,j;
-	
-	char *c=chars;
-	if(T.ch)
-		free(T.ch);
-	for(i=0;*c;++i,++c); 
-	if(!i)
-	{
-		T.ch=NULL;
-		T.length=0;
-	} 
+typedef int Status;
+ //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<串S的操作开始<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ 
+//串的定长顺序表示
+#define MAXSTRLEN 100
+ 
+typedef unsigned char SString[MAXSTRLEN+1];			//0号单元存放串的长度
+ 
+//生成一个其值等于chars的串T
+Status StrAssign(SString &T , char *chars)
+{
+	int i;
+	if(strlen(chars) > MAXSTRLEN) 
+		return ERROR;
 	else
 	{
-		T.ch=(char *)malloc(i*sizeof(char));
-		if(T.ch==NULL)
-			exit(OVERFLOW);
-		for(j=0;j<i;j++)
-		{
-			T.ch[j]=chars[j];
-		}
-		T.length=i;
+		T[0] = strlen(chars);
+		for(i = 1 ; i <= T[0] ; i++)
+			T[i] = *(chars+i-1);
+		return OK;
 	}
-	
-	return OK;
 }
 
-
-Status StrPrint(SString T)
-{
-	for(int i=0;i<T.length;i++)
-	{
-		printf("%c",T.ch[i]);
-	} 
-	printf("\n");
-	return OK;
+Status StrPrint(SString T){
+   int i;
+   for(i=1;i<=T[0];i++)
+    	printf("%c",T[i]);
+   printf("\n");
+   return OK;
+   
 }
 
-int StrLength(SString S)
-{
-	return S.length;
-} 
+int StrLength(SString S){
+	return S[0];
+}
 
-int StrCompare(SString S,SString T){
-	
+int StrCompare(SString S , SString T){
 	int i;
-	for(i=0;i<S.length && i<T.length;++i)
-	{
-		if(S.ch[i]!=T.ch[i])
-			return S.ch[i]-T.ch[i];
-	}
-	return S.length-T.length;
- } 
+	for(i = 1 ; i <= S[0] && i <= T[0] ; i++)
+		if(S[i] != T[i]) 
+			return S[i] - T[i];
+	return S[0]-T[0];
+}
 
 Status ClearString(SString &S){
-	
 	//将S串清空 
-	if(S.ch)
-	{
-		free(S.ch);
-		S.ch=NULL;
-	}
-	S.length=0;
+	S[0] = 0;
 	return OK;
-} 
-
-Status SubString(SString &Sub,SString S,int pos,int len){
-//	用Sub返回串S的第pos个字符起长度为len的子串 
-	if(pos<1 || pos>S.length ||len<0 || len>S.length-pos+1)
+}
+ 
+Status SubString(SString &Sub , SString S , int pos , int len){
+	//	用Sub返回串S的第pos个字符起长度为len的子串 
+	int i;
+	if(pos < 1 || pos > S[0] || len < 0 || len > S[0]-pos+1)
 		return ERROR;
-	if(Sub.ch)
-		free(Sub.ch);		//释放旧空间 
-	if(!len)
-		InitSString(Sub);
-	else					//完整子串 
-	{
-		Sub.ch=(char *)malloc(len*sizeof(char));
-		for(int i=0;i<len;i++)
-		{
-		 	Sub.ch[i]=S.ch[pos-1+i];
-		}
-		Sub.length=len; 
-	}
+	for(i = 1 ; i <= len ; i++)
+		Sub[i] = S[i+pos-1];	//注意此处是S[i+pos-1]，从第pos个字符
+	Sub[0] = len;
 	return OK;
 }
 
 Status StrEmpty(SString S){
-	if(!S.ch || S.length==0)
-		return OK;
+	if(S[0] == 0)
+		return TRUE;
 	else
-		return ERROR;
-} 
+		return FALSE;
+}
 
-Status StrCopy(SString &hstr,SString str){
-	if(!StrEmpty(str)){
-		for(int i=0;i<str.length;i++)
-			hstr.ch[i]=str.ch[i];
-		hstr.length=str.length;
-	}
-	return OK; 
+Status StrCopy(SString &T , SString S){
+	int i;
+	for(i = 0 ; i <= S[0] ; i++)
+		T[i] = S[i];
+	return OK;
 }
  
-Status serve(SString &str,SString &hstr){
+Status sever(SString &str, SString &hstr){
 	//将非空串str分割成两部分:hstr为第一个','之前的子串,str为那之后的子串 
-	int i=0,k=0;	//k为尚未匹配的左括号个数 
+	int i = 0, k = 0;//k为尚未匹配的左括号个数 
 	SString ch;
-	InitSString(ch);
-	
-	int n=StrLength(str);
+	int n = StrLength(str);
 	//搜索最外成的第一个逗号 
-	do{
+	do
+	{
 		++i;
-		SubString(ch,str,i,1);
-		if(ch.ch[0] == '(')
-			++k;
-		else if(ch.ch[0] == ')')
-			--k;
-	}while(i<n&&(ch.ch[0] != ',' || k != 0));
-	if(i<n){
-		SubString(hstr,str,1,i-1);
-		SubString(str,str,i+1,n-i);
+		SubString(ch, str, i, 1);
+		if (ch[1] == '(') ++k;
+		else if (ch[1] == ')') --k;
+	} while (i < n && (ch[1] != ',' || k != 0));
+	if (i < n) 
+	{
+		SubString(hstr, str, 1, i - 1);
+		SubString(str, str, i + 1, n - i);
 	}
-	else{
-		StrCopy(hstr,str);
+	else
+	{
+		StrCopy(hstr, str);
 		ClearString(str);
 	}
 	return OK;
-}
-
+} 
+ 
+ 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<广义表操作开始<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+typedef char AtomType;
+typedef int ElemType;
 
-typedef enum{ATOM,LIST}ElemTag;//ATOM == 0:原子,LIST == 1:子表 
-
-typedef SString AtomType;
-
+typedef enum {ATOM, LIST} ElemTag;		// ATOM == 0：原子 LIST == 1：子表
+ 
 typedef struct GLNode{
 	ElemTag tag;	//公共部分,用于区分原子结点和表结点
 	union{			//原子结点和表结点联合的部分 
@@ -166,104 +126,171 @@ typedef struct GLNode{
 		}ptr;		//ptr是表结点的指针域,ptr.hp和ptr.tp分别指向表头和表尾 
 	};  
 }*GList;			//广义表类型GList是一种头尾链表 
-
-Status CreateGList(GList &L,SString S){
+ 
+void InitList(GList &L)
+{
+	L = NULL;
+}
+ 
+void CreateGList(GList &L, SString S){
 	//采用头尾链表存储结构,有广义表的书写形式串S创建广义表L。设emp="()" 
-	SString emp,sub,hsub;
+	SString sub, hsub, emp;
+	GList p, q;
+	char chars[]="()";
+	StrAssign(emp, chars);
 	
-	InitSString(emp);
-	char chars[]="()"; 
-	
-	StrAssign(emp,chars);
-
-	InitSString(sub);
-	InitSString(hsub);
-	
-	GList p,q;
-	if(!StrCompare(S,emp)){//创建空表 
-		L=NULL;
-		printf("空"); 
-	}
-	else{
-		if(!(L = (GList)malloc(sizeof(GLNode))))//建表结点 
+	if (!StrCompare(S, emp)) //创建空表 
+		L = NULL;
+	else
+	{
+		L = (GList)malloc(sizeof(GLNode));
+		if (!L)				//建表结点 
 			exit(OVERFLOW);
-		if(StrLength(S) == 1){			//创建单原子广义表 
+		if (StrLength(S) == 1)	//创建单原子广义表 
+		{
 			L->tag = ATOM;
-			L->atom=S;
-		} 
-		else{
+			L->atom = S[1];
+		}
+		else
+		{
 			L->tag = LIST;
-			p=L;
-			SubString(sub,S,2,StrLength(S)-2);//脱去外层括号 
-			do{
+			p = L;
+			SubString(sub, S, 2, StrLength(S) - 2);		//脱去外层括号 
+			do
+			{
 				//重复建n个子表
-				serve(sub,hsub); //从sub中分离出表头串hsub
-				CreateGList(p->ptr.hp,hsub);
-				q=p;
-				if(!StrEmpty(sub)){//表尾不空 
-					if(!(p = (GList)malloc(sizeof(GLNode))))
+				sever(sub, hsub);//从sub中分离出表头串hsub
+				CreateGList(p->ptr.hp, hsub);
+				q = p;
+				if (!StrEmpty(sub))		//表尾不空 
+				{
+					p = (GList)malloc(sizeof(GLNode));
+					if (!p) 
 						exit(OVERFLOW);
 					p->tag = LIST;
 					q->ptr.tp = p;
 				}
-			}while(!StrEmpty(sub));
+			} while (!StrEmpty(sub));
 			q->ptr.tp = NULL;
-		} 
-	} 
-	return OK; 
-}
-
-int GListDepth(GList L){
-	//采用头尾链表存储结构,求广义表L的深度
-	if(!L)
-		return 1;		//空表深度为1
-	if(L->tag == ATOM)
-		return 0;		//原子深度为0
-	GList pp;
-	int max,dep;
-	for(max = 0,pp = L;pp;pp=pp->ptr.tp){
-		dep = GListDepth(pp->ptr.hp);//求以pp->ptr.hp为头指针的子表深度
-		if(dep > max)
-			max = dep; 
+		}
 	}
-	return max+1;
-}
-
-Status CopyGList(GList &T,GList L){
-	//采用头尾链表存储结构,由广义表L复制得到广义表T
-	if(!L)
-		T = NULL;
-	else{
-		if(!(T = (GList)malloc(sizeof(GLNode))))
-			exit(OVERFLOW);
-		T->tag = L->tag;
-		if(L->tag == ATOM)
-			T->atom = L->atom;		//复制单原子
-		else{
-			CopyGList(T->ptr.hp,L->ptr.hp);
-			CopyGList(T->ptr.tp,L->ptr.tp);
-		} 
-	} 
-	return OK;
 }
  
-int main(int argc, char** argv) {
-	
-	SString S;
-	InitSString(S);
-	char chars[100];
-	printf("请输入一个广义表:");
+int GListLength(GList L)
+{
+	int len = 0;
+	while (L)
+	{
+		L = L->ptr.tp;
+		++len;
+	}
+	return len;
+}
+ 
+int GListDepth(GList L){
+	//采用头尾链表存储结构,求广义表L的深度
+	int max, dep;
+	GList pp;
+	if (!L) 		//空表深度为1
+		return 1;
+	if (L->tag == ATOM) 	//原子深度为0
+		return 0;
+	for (max = 0, pp = L; pp; pp = pp->ptr.tp)
+	{
+		dep = GListDepth(pp->ptr.hp);//求以pp->ptr.hp为头指针的子表深度
+		if (dep > max) 
+			max = dep;
+	}
+	return max + 1;
+}
+
+ 
+void CopyGList(GList &T, GList L){
+	//采用头尾链表存储结构,由广义表L复制得到广义表T
+	if (!L) T = NULL;		// 复制空表
+	else
+	{
+		T = (GList)malloc(sizeof(GLNode));
+		if (!T) exit(OVERFLOW);
+		T->tag = L->tag;
+		if (L->tag == ATOM)
+			T->atom =L->atom;	//复制单原子
+		else
+		{
+			CopyGList(T->ptr.hp, L->ptr.hp);
+			CopyGList(T->ptr.tp, L->ptr.tp);
+		}
+	}
+}
+ 
+ 
+  
+void DestroyGList(GList &L)
+{
+	GList q1, q2;
+	if (L)
+	{
+		q1 = L->ptr.hp;
+		q2 = L->ptr.tp;
+		DestroyGList(q1);
+		DestroyGList(q2);
+	}
+	free(L);
+	L =  NULL;
+}
+
+ 
+GList GetHead(GList L)
+{
+	GList h, p;
+	if (!L)
+		return NULL;
+	p = L->ptr.hp;
+	CopyGList(h, p);
+	return h;
+}
+ 
+GList GetTail(GList L)
+{
+	GList h, p;
+	if (!L)
+		return NULL;
+	p = L->ptr.tp;
+	CopyGList(h, p);
+	return h;
+}
+ 
+ 
+ 
+int main(int argc,char**argv){
+ 	SString S;
+ 	char chars[100]; 
+ 	
+	printf("请输入广义表T:");
 	gets(chars);
 	StrAssign(S,chars);
-	printf("广义表输出:"); 
+	printf("广义表T输出:"); 
 	StrPrint(S);
 	
 	GList L,T;
-	CreateGList(L,S);
+	InitList(L);
+	CreateGList(L,S); 
+	int deepth=GListDepth(L);
+	printf("广义表T的深度为:%d\n",deepth);
 	
-	int deep = GListDepth(L);
-	printf("广义表的深度为:%d",deep);
-	return 0;
-}
-
+	int length = GListLength(L);
+	printf("广义表的T长度为:%d\n",length);
+	
+	printf("开始复制广义表T到L\n");
+	InitList(T);
+	CopyGList(T,L);
+	
+	int deepth1=GListDepth(L);
+	printf("广义表L的深度为:%d\n",deepth1);
+	
+	int length1 = GListLength(L);
+	printf("广义表的L长度为:%d\n",length1);
+ 	return 0;
+ } 
+ 
  
